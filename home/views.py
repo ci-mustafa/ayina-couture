e2e55114509e1e8abb2d6e57a48e2a6ae809cf91
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-12-31 19:35:56 +0000

    feat: add home page

---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Home page view
+def index(request):
+    """
+    View function for rendering the home page of the site.
+
+    This view is responsible for rendering the 'home/index.html' template 
+    which represents the main landing page of the website.
+
+    Args:
+        request (HttpRequest): The request object that contains metadata 
+        about the request.
+
+    Returns:
+        HttpResponse: The rendered HTML page for the home page.
+    """
+    return render(request, 'home/index.html')
