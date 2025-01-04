34d92fad6838ec8901cd1500434353416a37bde1
ci-mustafa akbari <akbarimustafa64@gmail.com>
2025-01-04 13:27:57 +0000

    feat: display stars rating for each product

--- products/views.py
+++ products/views.py
@@ -5,7 +5,9 @@ from . import models
 # Products view
 def all_products(request):
     products = models.Product.objects.all()
+    ratings_range = range(1, 6)  # range from 1 to 5 for the stars
     context = {
-        'products': products
+        'products': products,
+        'rating_range': ratings_range
     }
     return render(request, 'products/products.html', context)
