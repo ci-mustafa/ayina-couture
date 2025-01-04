b7ee1494c1de3e24dee6c218a30bb6f5a4809683
ci-mustafa akbari <akbarimustafa64@gmail.com>
2025-01-04 12:31:46 +0000

    feat: query all products and style them a bit

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+# Products view
+def all_products(request):
+    products = models.Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products/products.html', context)
