63f43c4b1bebd7249b186ab581309712cac6d103
ci-mustafa akbari <akbarimustafa64@gmail.com>
2025-01-04 13:38:12 +0000

    feat: add a fixed rate value to the view

--- products/views.py
+++ products/views.py
@@ -6,8 +6,10 @@ from . import models
 def all_products(request):
     products = models.Product.objects.all()
     ratings_range = range(1, 6)  # range from 1 to 5 for the stars
+    top_rate_value = 5.0
     context = {
         'products': products,
-        'rating_range': ratings_range
+        'rating_range': ratings_range,
+        'top_rate_value': top_rate_value
     }
     return render(request, 'products/products.html', context)
