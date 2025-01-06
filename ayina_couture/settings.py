4cb9c27e73c36dd00e141c83c6f602c199943f0c
Mustafa Akbari <akbarimustafa64@gmail.com>
2025-01-06 09:30:27 +0000

    feat: add context proccesor for cart app , available all it's context data throgh the project

--- ayina_couture/settings.py
+++ ayina_couture/settings.py
@@ -107,6 +107,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'cart.contexts.cart_context',
             ],
         },
     },
---
+++ cart/contexts.py
@@ -0,0 +1,10 @@
+
+
+
+def cart_context(request):
+    
+    context = {
+
+    }
+
+    return context
