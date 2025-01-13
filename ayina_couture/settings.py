0ef7ac5300e59145398fbe08938da3fcb3e5e8a5
Mustafa Akbari <akbarimustafa64@gmail.com>
2025-01-13 20:20:43 +0000

    feat: add whitenoise package

--- ayina_couture/settings.py
+++ ayina_couture/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
