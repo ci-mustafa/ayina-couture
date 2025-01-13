ec812af180cec9d84f75db5cd874c9e5885fb811
Mustafa Akbari <akbarimustafa64@gmail.com>
2025-01-13 20:55:01 +0000

    mod: update setting file

--- staticfiles/js/show_toasts.js
+++
@@ -1,8 +0,0 @@
-// Check if there are any messages and display the toast
-$(document).ready(function() {
-    // Display all toasts
-    var toasts = document.querySelectorAll('.toast');
-    toasts.forEach(function(toast) {
-        new bootstrap.Toast(toast).show(); // Show the toast using Bootstrap
-    });
-});
