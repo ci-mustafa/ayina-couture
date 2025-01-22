5173e86702b778c583db582c9300ef74001e0b21
Mustafa Akbari <akbarimustafa64@gmail.com>
2025-01-22 16:09:56 +0000

    feat: add wishlist model, register it to admin site

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+# Wishlist admin
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['user_username', 'product_name', 'created_at']
+
+    def user_username(self, obj):
+        return obj.user.username
+    user_username.short_description = "User"
+
+    def product_name(self, obj):
+        return obj.product.name
+    product_name.short_description = "Product"
+
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+# Wishlist model
+class Wishlist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='wishlist')
+    products = models.ManyToManyField(Product, related_name='wishlists')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Wishlist of {self.user.username}"
+
+
