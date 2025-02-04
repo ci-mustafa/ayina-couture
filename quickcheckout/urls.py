55fd55c5732f42ce648a1db0fea07200c679af7f
Mustafa Akbari <akbarimustafa64@gmail.com>
2025-02-04 18:31:22 +0000

    feat: add webhooks

--- quickcheckout/urls.py
+++ quickcheckout/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import quickcheckout, checkout_success
+from .webhooks import webhook
 urlpatterns = [
     path('', quickcheckout, name='quickcheckout'),
     path('checkout_success/<order_number>', checkout_success, name='checkout-success'),
+    path('wh/', webhook, name='webhook'),
 ]
